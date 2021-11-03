9fa2d7d7c9159b300cabbc24a5dde604a9d6bc41
wcgordon1 <wcgordon1@gmail.com>
2021-11-03 12:20:47 -0700

    got it working

--- new/main.js
+++ new/main.js
@@ -4,39 +4,65 @@ function saveIssue(e) {
     var issueDesc = document.getElementById('issueDescInput').value;
     var issueSeverity = document.getElementById('issueSeverityInput').value;
     var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-    var issueID = chance.guid();
+    var issueId = chance.guid();
     var issueStatus = 'Open';
 
     var issue = {
-        id: issueID,
+        id: issueId,
         description: issueDesc,
         severity: issueSeverity,
         assignedTo: issueAssignedTo,
-        status: issueStatus,
+        status: issueStatus
     }
 
     if (localStorage.getItem('issues') == null) {
         var issues = [];
         issues.push(issue);
-        localStorage.setItem('issues', JSO.stringify(issues));
+        localStorage.setItem('issues', JSON.stringify(issues));
     } else {
         var issues = JSON.parse(localStorage.getItem('issues'));
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
     }
 
-    document.getElementById('isseInputForm').reset();
+    document.getElementById('issueInputForm').reset();
 
     fetchIssues();
 
     e.preventDefault();
+}
 
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
 
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = 'Closed';
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
+function deleteIssue(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
 }
 
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesList = document.getElementById('issuesList');
+    var issuesListe = document.getElementById('issuesList');
 
     issuesList.innerHTML = '';
 
@@ -51,9 +77,9 @@ function fetchIssues() {
             '<h6>Issue ID: ' + id + '</h6>' +
             '<p><span class="label label-info">' + status + '</span></p>' +
             '<h3>' + desc + '</h3>' +
-            '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-            '<p><span class="glyphicon glyphicon-user"></span>' + assignedTO + '</p>' +
-            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a>' +
+            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>' +
+            '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
+            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a> ' +
             '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +
             '</div>';
     }
